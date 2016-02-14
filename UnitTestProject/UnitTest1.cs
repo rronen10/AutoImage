c04a4115e41140bdfddcd95cf6e3b8b760379343
ronenr <ronenr@perfectomobile.com>
2016-02-14 10:31:49 +0200

    add nuspec

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Drawing;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using System;
+//using System.Drawing;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        private readonly string imageTest = @"C:\TestProjects\ImageBaseTest\UnitTestProject\template.png";
-        [TestMethod]
-        public void TestFind()
-        {
-            Assert.IsTrue(ImageBaseTest.ImageTestUtil.Exists(imageTest));
-        }
+//namespace UnitTestProject
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        private readonly string imageTest = @"C:\TestProjects\ImageBaseTest\UnitTestProject\template.png";
+//        [TestMethod]
+//        public void TestFind()
+//        {
+//            Assert.IsTrue(ImageBaseTest.ImageTestUtil.Exists(imageTest));
+//        }
 
-        [TestMethod]
-        public void TestClick()
-        {
-            ImageBaseTest.ImageTestUtil.Click(imageTest);
-        }
+//        [TestMethod]
+//        public void TestClick()
+//        {
+//            ImageBaseTest.ImageTestUtil.Click(imageTest);
+//        }
 
-        [TestMethod]
-        public void TestClickOffset()
-        {
-            ImageBaseTest.ImageTestUtil.Click(imageTest,new Size(0,0));
-        }
+//        [TestMethod]
+//        public void TestClickOffset()
+//        {
+//            ImageBaseTest.ImageTestUtil.Click(imageTest,new Size(0,0));
+//        }
 
-        [TestMethod]
-        public void TestRightClick()
-        {
-            ImageBaseTest.ImageTestUtil.Click(imageTest,isRightClick:true);
-        }
-    }
-}
+//        [TestMethod]
+//        public void TestRightClick()
+//        {
+//            ImageBaseTest.ImageTestUtil.Click(imageTest,isRightClick:true);
+//        }
+//    }
+//}
